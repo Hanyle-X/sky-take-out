3849d253f400440f63d5c8dd7f89fb46b32f86ef
Hanyle-X <2967233162@qq.com>
2025-11-20 23:24:26 +0800

    新增员工业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,34 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    /**
+     * 插入员工数据
+     * @Param employee
+     */
+    @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) " +
+            "values(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
+    void insert(Employee employee);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+}
